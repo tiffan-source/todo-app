4287f5a8afccae248fccd76e41eff0ce9a31dc6c
tiffan-source <zohoungbogboanlyou@gmail.com>
2025-06-27 21:20:01 +0200

    chore: config husky, prettier, commit lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
+    expoConfig,
+    eslintPluginPrettierRecommended,
+    {
+        ignores: ["dist/*"],
+    },
 ]);
